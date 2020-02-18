ac5f2dc26c9f4aad9a4aa004c15a1bcba1eae356
Jonathan <jonathan.spiller@gmail.com>
2020-02-18 12:32:46 +0200

    Added Online store functionality

--- app.py
+++ app.py
@@ -1,20 +1,91 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 
 import db
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
 
 @app.route('/')
 def home():  # CONTROLLER
+    return render_template('home.html')
+
+
+
+@app.route('/products')
+def products():  # CONTROLLER
 
     # MODEL
     products = db.load_database()
 
     # VIEW
-    html = render_template('home.html', products=products)
+    return render_template('products.html', products=products)
 
-    return html
+
+@app.route('/product_details/<int:product_id>')
+def product_details(product_id):  # CONTROLLER
+
+    # MODEL
+    products = db.load_database()
+
+
+    # VIEW
+    return render_template('product_details.html', product=products[product_id])
+
+
+@app.route('/buy/<int:product_id>')
+def buy(product_id):  # CONTROLLER
+
+    # MODEL
+    products = db.load_database()
+    products[product_id]['stock'] -= 1
+    db.update_database(products)
+
+    return redirect(f'/product_details/{product_id}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from flask import request
+from forms import *
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+
+    login_form = LoginForm()
+
+    if request.method == 'GET':
+        return render_template('login.html', form=login_form)
+
+
+    if request.method == 'POST' and login_form.validate():
+        name = login_form.name.data
+        password = login_form.password.data
+        info = login_form.info.data
+
+        if name == "Jonathan" and password == "horse":
+            return f"<h1>Welcome Jon. Your info is {info}.</h1>"
+        else:
+            return "<h1>Access Denied</h1>"
+
+    else:
+        return "<h1>Failed Validation</h1>"
+
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -3,28 +3,36 @@ import json
 def create_database(dst_file='my_file.json'):
     data = [
         {
+            'id': 0,
             'name': 'iphone 9',
             'price': 799,
-            'instock': True
+            'stock': 3,
+            'image': 'iphone9.jpeg'
         },
 
         {
+            'id': 1,
             'name': 'Macbook Pro',
             'price': 4799,
-            'instock': False
+            'stock': 0,
+            'image': 'macbookpro.jpeg'
         },
 
         {
+            'id': 2,
             'name': 'Apple Watch',
             'price': 10799,
-            'instock': True
+            'stock': 3,
+            'image': 'applewatch.jpeg'
         },
 
         {
-            'name': 'Apple stand pro',
+            'id': 3,
+            'name': 'Android Phone',
             'price': 99999,
-            'instock': True
-        },
+            'stock': 5,
+            'image': 'androidphone.jpeg'
+        }
     ]
 
     with open(dst_file, 'w') as f:
@@ -35,6 +43,10 @@ def load_database(src_file='my_file.json'):
         data = json.load(f)
     return data
 
+def update_database(products, src_file='my_file.json'):
+    with open(src_file, 'w') as f:
+        json.dump(products, f, indent=4)
+        print("database updated...")
 
 
 
